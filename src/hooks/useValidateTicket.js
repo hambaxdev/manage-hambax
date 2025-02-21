886f55c36793f6d5b58968def3dce011b1578eb7
hambaxdev <hambax.dev@gmail.com>
2025-02-21 11:38:21 +0100

    fix api path

--- src/hooks/useValidateTicket.js
+++ src/hooks/useValidateTicket.js
@@ -18,7 +18,7 @@ const useValidateTicket = () => {
         }
 
         try {
-            const response = await fetch(`${process.env.REACT_APP_HAMBAX_NEW_API_URL}/api/tickets/${qrHash}`, {
+            const response = await fetch(`${process.env.REACT_APP_HAMBAX_NEW_API_URL}/api/tickets/validate/${qrHash}`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
